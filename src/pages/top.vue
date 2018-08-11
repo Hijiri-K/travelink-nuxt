07980459e70d7c48c35790b0a8904ff883195bd8
Hijiri K <hijiri.k.emk.p@gmail.com>
2018-08-12 00:11:44 +0900

    topページの仮作成

--- src/pages/top.vue
+++ src/pages/top.vue
@@ -2,45 +2,98 @@
 
 <template>
   <div class="">
-    <h1>Travelink</h1>
-    <p>Travel cordinate app</p>
-    <p>ここはtop画面なので、椎野よろしく。</p>
-    <p>pages\top.vue</p>
-
-    <div id="app">
-        <router-link to="/planning">Go to planning</router-link>
+    <div class="locations-wrapper" :style="{ height: pageHeight }">
+      <el-row type="flex" class="locations-row" :gutter="10" >
+        <el-col :span=12 class="location">
+          <div class="location">
+            <el-card :body-style="{ padding: '0px' }" class="location-pictures">
+              <img src="https://cdn.4travel.jp/img/tcs/t/album/src/10/40/47/src_10404771.jpg?1259494610">
+            </el-card>
+          </div>
+        </el-col>
+        <el-col :span=12 class="location">
+          <div class="location">
+            <el-card :body-style="{ padding: '0px' }" class="location-pictures">
+              <img src="https://cdn.4travel.jp/img/tcs/t/album/src/10/40/47/src_10404771.jpg?1259494610">
+            </el-card>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row type="flex" class="locations-row" :gutter="10">
+        <el-col :span=12 class="location">
+          <div class="location">
+            <el-card :body-style="{ padding: '0px' }" class="location-pictures">
+              <img src="https://cdn.4travel.jp/img/tcs/t/album/src/10/40/47/src_10404771.jpg?1259494610">
+            </el-card>
+          </div>
+        </el-col>
+        <el-col :span=12 class="location">
+          <div class="location">
+            <el-card :body-style="{ padding: '0px' }" class="location-pictures">
+              <img src="https://cdn.4travel.jp/img/tcs/t/album/src/10/40/47/src_10404771.jpg?1259494610">
+            </el-card>
+          </div>
+        </el-col>
+      </el-row>
     </div>
-
+    <div class="">
+      <el-button class="shuffle-btn" type="success" round>Shuffle</el-button>
+    </div>
   </div>
-
 </template>
 
 <style>
+.locations-wrapper{
+  box-sizing:border-box;
+  margin-top: 40px;
+  padding:10px 10px 10px 10px;
+  /* height:80%; */
+  width:100%;
+}
 
+.locations-row{
+  /* box-sizing:border-box; */
+  height:50%;
+  margin:0 0 10px 0;
+}
+
+.location{
+  /* margin:20px; */
+  height:100%;
+}
+
+.location-pictures{
+  /* height:100%; */
+  /* background-color: green; */
+  height:100%;
+
+}
+
+img{
+  max-height:100%;
+  width:100%;
+  vertical-align: text-bottom;
+}
+
+.shuffle-btn{
+  width:98%;
+  margin: 10px;
+}
 </style>
+
 <script>
-// import Vue from 'vue'
-// import VueRouter from 'vue-router'
-// import Planning from '../pages/planning.vue'
-//
-// // const Foo = { template: '<div>foo</div>' }
-// // const Bar = { template: '<div>bar</div>' }
-//
-// // 2. ルートをいくつか定義します
-// // 各ルートは 1 つのコンポーネントとマッピングされる必要があります。
-// // このコンポーネントは実際の `Vue.extend()`、
-// // またはコンポーネントオプションのオブジェクトでも構いません。
-// // ネストされたルートに関しては後で説明します
-// const routes = [
-//   { path: '/planning', component: Planning },
-// ]
-//
-// const router = new VueRouter({
-//   routes // `routes: routes` の短縮表記
-// })
-//
-// const app = new Vue({
-//   router
-// }).$mount('#app')
+import Vue from 'vue/dist/vue.esm'
+var pageHeight;
+export default{
+  data() {
+    return {
+      pageHeight: pageHeight
+    }
+  },
+  mounted: function(){
+    this.pageHeight = window.parent.screen.height - 110 + 'px';
+    console.log(this.pageHeight)
+  }
+}
 
 </script>
