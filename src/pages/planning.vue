b8ec2bb6c1348618db4e457c060af7790d71da82
Hijiri K <hijiri.k.emk.p@gmail.com>
2018-08-16 01:14:08 +0900

    修正により大きなバグが発生したため、修正を削除

--- src/pages/planning.vue
+++ src/pages/planning.vue
@@ -534,7 +534,7 @@ export default {
                 }
 
                 //numsOfDailyPlacesがundefinedの場合（itineraryのcalcurateRouteAfterDragging以外から呼び出された場合）
-                if (numsOfDailyPlaces == undefined || typeof numsOfDailyPlaces == "object" ) { //TODO:editの後にoptimizeが押されるとnumsOfDailyPlacesにundefinedではなく、[object Event]が入ってくるので、この場合もtrueにしたい。（修正済み）
+                if (numsOfDailyPlaces == undefined) { //TODO:editの後にoptimizeが押されるとnumsOfDailyPlacesにundefinedではなく、[object Event]が入ってくるので、この場合もtrueにしたい。
 
                   if(totalDuration >= DAILY_MAX_TOTAL_TIME || i == selectedPlaces.length - 1){
                     planningPlaces.push(dailyPlanningPlaces);
@@ -568,7 +568,7 @@ export default {
               }
 
 
-            if (numsOfDailyPlaces == undefined  || typeof numsOfDailyPlaces == "object") {　//TODO:editの後にoptimizeが押されるとnumsOfDailyPlacesにundefinedではなく、[object Event]が入ってくるので、この場合もtrueにしたい。(修正済み)
+            if (numsOfDailyPlaces == undefined) {　//TODO:editの後にoptimizeが押されるとnumsOfDailyPlacesにundefinedではなく、[object Event]が入ってくるので、この場合もtrueにしたい。
 
             //最短距離のホテルの検索
             // DistanceMatrix サービスを生成
