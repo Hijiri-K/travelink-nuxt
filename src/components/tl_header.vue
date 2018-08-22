f8fec6cb32cef4aa4f25dda126decda600161ed9
Hijiri K <hijiri.k.emk.p@gmail.com>
2018-08-22 17:24:38 +0900

    headerの改善

--- src/components/tl_header.vue
+++ src/components/tl_header.vue
@@ -1,23 +1,37 @@
 <template>
-<div>
-  <a class="header__logo">Travelink</a>
+<div class="header-wrapper">
+  <router-link tag="span" class="header__logo" to="/top">Travelink</router-link>
   <ul class="header__menu">
-    <li class="header__menu">about</li>
-    <li class="header__menu">mypage</li>
+    <!-- <li class="header__menu">about</li>
+    <li class="header__menu">mypage</li> -->
   </ul>
 </div>
 </template>
 
 <style scoped>
-.header__menu{
-  display: inline-block;
-  /* font-size: 20px; */
+.header-wrapper{
+  height:100%;
 }
 
 .header__logo{
   display: inline-block;
-  color:red;
-  /* font-size: 24px; */
+  color:#fff;
+  font-size: 24px;
+  height:100%;
+  line-height: 40px;
+  opacity: .9;
+}
+
+.header__menu{
+  display: inline-block;
+  font-size: 18px;
+  height:100%;
+  line-height: 40px;
+  vertical-align: top;
+}
+
+.header__menu li{
+margin: 0 10px;
 }
 
 ul{
